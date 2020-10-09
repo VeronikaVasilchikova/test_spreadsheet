410c9ef5d3abf73070a6db6e4377fe061735b92a
Veronika <v.vasilchykava@gmail.com>
2020-10-09 08:43:49 +0300

    Create the app using webix spreadcheet, custom, button, modal window

---
+++ data/categories.js
@@ -0,0 +1,23 @@
+// const categories = [
+//   {id: 1, value: ""},
+//   {id: 2, value: "AA"},
+//   {id: 3, value: "BB"},
+//   {id: 4, value: "CC"},
+//   {id: 5, value: "DD"},
+//   {id: 6, value: "EE"},
+//   {id: 7, value: "FF"},
+//   {id: 8, value: "GG"}
+// ];
+
+const categories = new webix.DataCollection({
+	data: [
+		{id: 1, value: ""},
+    {id: 2, value: "AA"},
+    {id: 3, value: "BB"},
+    {id: 4, value: "CC"},
+    {id: 5, value: "DD"},
+    {id: 6, value: "EE"},
+    {id: 7, value: "FF"},
+    {id: 8, value: "GG"}
+	]
+});
